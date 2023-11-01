a3456478feae900fcc5422db50a025553164716c
manishSanodiya <122765253+manishSanodiya@users.noreply.github.com>
2023-11-01 21:31:36 +0530

    error last

--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -12,9 +12,12 @@ const Searched = () => {
 
 
     const GetCuisine = async(name)=>{
-        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${name}`);
+      try{  const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&query=${name}`);
         const recipes =await data.json();
         setSearch(recipes.results);
+    }catch(err){
+        console.error("ERROR: -" , err);
+    }
     }
     
     useEffect(()=>{
